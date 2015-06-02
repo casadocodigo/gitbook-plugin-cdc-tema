760eea38ef82c6632c1561ac3da38a7312e3da02
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2015-06-02 17:25:31 -0300

    minor

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ module.exports = {
                 this.options.pdf.margin.right =  "62";
 
                 this.options.pdf.headerTemplate = "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>_SECTION_</span></span><span class='even_page'><span>_SECTION_</span><span style='float:right'>Casa do Código</span></span><script>if(!(/^[0-9]/.test('_SECTION_'))) { document.getElementById('ebook-header').style.display='none'; }</script></p>";
-
                 this.options.pdf.footerTemplate = "<p id='ebook-footer'></p><script>var footer = document.getElementById('ebook-footer'); footer.innerHTML = _PAGENUM_ - 2; if(_PAGENUM_ % 2 != 0){ footer.style.textAlign = 'right'; }</script>";
                 
-                this.options.pdf.summary = {};
-                this.options.pdf.summary.headerTemplate = "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>Sumário</span></span><span class='even_page'><span>Sumário</span><span style='float:right'>Casa do Código</span></span></p>";
-                
-                this.options.pdf.summary.footerTemplate = null;
+                this.options.pdf.summary = {
+                    headerTemplate: "<p id='ebook-header' style='border-bottom: 1px solid black; margin-top: 36pt;'><span class='odd_page'><span>Casa do Código</span><span style='float:right'>Sumário</span></span><span class='even_page'><span>Sumário</span><span style='float:right'>Casa do Código</span></span></p>",
+                    footerTemplate: null
+                };
             }
     }
     , ebook: theme.ebook
