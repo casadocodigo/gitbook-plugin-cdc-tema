269bdd79677e1362374c3b8c08d41a44058f88fa
Adriano Almeida <adrianoalmeida7@gmail.com>
2015-08-20 15:23:42 -0300

    MaxLineLength is 67

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var theme = require("./theme");
 module.exports = {
     hooks: {
         init: function(){
-			this.options.maxLineLength = 80;
+	    this.options.maxLineLength = 67;
             theme.pdfOptions.call(this);
         }
     }
